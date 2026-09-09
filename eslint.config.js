e1a898458e8d13642bf509686511e1122591515f
Alex Naskidashvili <lexo.naskidashvili@gmail.com>
2026-09-10 00:03:23 +0400

    Exclude contracts/ (vendored Foundry deps) from root ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,13 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["**/dist/**", "**/.next/**", "**/.turbo/**", "**/node_modules/**"],
+    ignores: [
+      "**/dist/**",
+      "**/.next/**",
+      "**/.turbo/**",
+      "**/node_modules/**",
+      "contracts/**", // Solidity + a vendored Foundry dependency tree, not ours to lint
+    ],
   },
   ...tseslint.configs.recommended,
 );
